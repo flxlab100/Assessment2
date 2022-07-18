795f3af4e42e5a19a194edacc9d911d5f465602c
flxlab100 <flxqlab@gmail.com>
2022-07-18 14:42:48 +0930

    3files updated 2

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const newTaskNameInput = document.querySelector('#newTaskNameInput');
+ 
+  function Numofadults(){
+    let x = document.getElementById("numofadults").value;
+    // If x is Not a Number or less than one or greater than 10
+    let text;
+    if (isNaN(x) || x < 1 || x > 3) {
+      text = "Input not valid";
+    } else {
+      text = "Input OK";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
+
+  var Name1=document.getElementById("name1");
+  var Name2=document.getElementById("name2");
+  var limit=5;
+  Name1.textContent=0+"/"+limit;
+  Name1.addEventListener("input",function(){
+      Name1.textContent=textLength+"/"+limit
+  })
