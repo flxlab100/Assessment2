698a5a70e9d8ef4be7059b16d97022ae22f08491
flxlab100 <flxqlab@gmail.com>
2022-07-19 15:50:57 +0930

    update9++

--- index.js
+++ index.js
@@ -11,56 +11,16 @@
     document.getElementById("demo").innerHTML = text;
   }
 
-  const groupforms=document.getElementById('groupforms');
-  const fullname=document.getElementById('fullname');
-  const email=document.getElementById('email');
-  const mobile=document.getElementById('mobile');
+  function confirmInput(){
+    let y = document.getElementById("fullname").value;
 
-  groupforms.addEventListener("submit", e=>{
-      e.preventDefault();
-      validateInputs();
-  });
-
-const setError=(element, message)=>{
-  const inputControl=elemtn.parentElement;
-  const errorDisplay=inputControl.querySelector('.error');
-
-  errorDisplay.innerText=message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success')
+       if(y.length<=5){
+        alert('hello'+ y.length);
+      return false;
+       } 
 }
 
-const setSuccess=element=>{
-  const inputControl=element.parentElement;
-}
+ 
 
 
-const validateInputs=()=>{
-      const  fullnameValue=fullname.value.trim();
-      const emailValue=email.value.trim();
-      const  mobileValue=mobile.value.trim();
-
-      if(fullnameValue===''){
-           setError=(fullname, "Fullname is required");
-      }else{
-        setSuccess(fullname);
-      }
-
-      if(emailValue===''){
-        setError(email, "Email is required");
-
-      }else if (!isValidEmail(emailValue)){
-        setError(email, 'Provide a valid email address');
-      }else{
-        setSuccess(email);
-      }
-
-      if(mobileValue===''){
-        setError(mobile, 'Mobile is not valid');
-      }else (mobile.length=10){
-        setSuccess(mobile);
-      } 
-         
-      }
-
-       
+  
