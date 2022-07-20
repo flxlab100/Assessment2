720a247a380129d62953b8abf718ec15f7435e0a
flxlab100 <flxqlab@gmail.com>
2022-07-20 14:28:07 +0930

    update11

--- index.js
+++ index.js
@@ -1,9 +1,65 @@
+function buttonclicked(){
+  alert("you have submitted");
+}
 
+function showMessage(input, message, type){
+  const msg=input.parentNode.querySelector('small');
+  msg,innerText=message;
+  input.className=type? "success":"error";
+  return type;
+}
+
+function showError(input, message) {
+	return showMessage(input, message, false);
+}
+
+function showSuccess(input) {
+	return showMessage(input, "", true);
+}
+
+function hasValue(input, message) {
+	if (input.value.trim() === "") {
+		return showError(input, message);
+	}
+	return showSuccess(input);
+}
+
+function validateEmail(input, requiredMsg, invalidMsg) {
+	// check if the value is not empty
+	if (!hasValue(input, requiredMsg)) {
+		return false;
+	}
+	// validate email format
+	const emailRegex =
+		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const email = input.value.trim();
+	if (!emailRegex.test(email)) {
+		return showError(input, invalidMsg);
+	}
+	return true;
+}
+
+
+const gform=document.querySelector('#groupforms');
 const fullname=document.querySelector('#fullname');
 const email=document.querySelector('#email');
 const mobile=document.querySelector("#mobile");
-const gform=document.querySelector('#groupforms');
 
-gform.addEventListener('submit', function (e) {
-  e.preventDefault();
+
+gform.addEventListener('submit', (event) =>{
+  e.preventDefault(); //stop form submission
+  let nameValid=hasValue(gform.elements["fullname"], NAME_REQUIRED);
+  let emailValid=validateEmail(gform.elements["email"],EMAIL_REQUIRED, EMAIL_INVALID);
+  let mobileValid=validateMobile(gform.elements["mobile"], MOBILE_REQUIRED);
+   if(nameValid && emailValid && mobileValid){
+    alert("demo only. no form was posted.");
+   }
 });
+
+gform.addEventListener('submit', (event) =>{
+   //handle the form data
+});
+
+
+ 
